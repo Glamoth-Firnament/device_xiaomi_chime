95bf9a070cafb188513eab98a48a0cc8ab8d1f40
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2023-03-11 15:36:39 +0700

    chime: rice: Build Core Gapps
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- lineage_chime.mk
+++ lineage_chime.mk
@@ -25,6 +25,7 @@ TARGET_ENABLE_BLUR := false
 TARGET_HAS_UDFPS := false
 TARGET_USE_PIXEL_FINGERPRINT := false
 TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+TARGET_CORE_GMS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_chime
