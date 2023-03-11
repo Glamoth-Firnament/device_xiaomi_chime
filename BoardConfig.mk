3ee20bfa0b72cd7ee06179132ed902a7348e8697
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2023-03-11 17:39:18 +0700

    juice: board: Use Greenforce Kernel
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,8 +107,10 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/chime
-TARGET_KERNEL_CONFIG := vendor/chime_defconfig
+TARGET_KERNEL_CONFIG := vendor/juice-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton/bin/aarch64-linux-gnu-
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"  AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1
