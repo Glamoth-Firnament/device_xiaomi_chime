ab8a1ea4ada4ff8019fc0702298f36cc4af6fa89
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2023-06-03 07:52:32 +0700

    Evolution Bringup!
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- evolution_chime.mk
+++ evolution_chime.mk
@@ -11,13 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chime device
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
-# Inherit some common derp Flags.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common evolution Flags.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-WITH_GMS := false
+TARGET_USES_PICO_GAPPS := true
+TARGET_ENABLE_BLUR := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_chime
+PRODUCT_NAME := evolution_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
