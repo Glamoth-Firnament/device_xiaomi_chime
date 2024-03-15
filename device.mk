321e907944e47dc288fef464fa8b684e96cdbf90
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2024-03-15 09:48:50 +0700

    chime: Build on-tree vibrator impl
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -613,7 +613,7 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.chime \
-    vendor.qti.hardware.vibrator-impl
+    vendor.qti.hardware.vibrator.impl.chime
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
